cac2c95bab00576a3fdf199e9f8372141cb0b433
Forz70043 <alfonso.pisicchio@libero.it>
2020-07-12 12:56:41 +0200

    Added package: services, ThemeData funct. & SystemChrome for set statusBar color

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,8 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.white));
+
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      
       /* FLUTTER DEFAULT 
         title: 'Flutter Demo',
         theme: ThemeData(

ccfe0ee11bcdc2383cf0935e221f7ba6d1d05bbb
Forz70043 <alfonso.pisicchio@libero.it>
2020-07-13 15:22:28 +0200

    Added package Contact and use it

--- my_app/lib/routes/routes.dart
+++ my_app/lib/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:contactus/contactus.dart';
 
 class Routes extends StatelessWidget {
 
@@ -10,8 +11,45 @@ class Routes extends StatelessWidget {
     return new Scaffold(
       appBar: new AppBar(title: new Text(route)),
       body: new Center(
-        child: new Text(route)
+        child: ContactUs(
+          logo: AssetImage('assets/images/ap-logo/AP-dev1.png'),
+          email: 'alfonso.pisicchio@libero.it',
+          githubUserName: 'Forz70043',
+          linkedinURL: 'https://www.linkedin.com/in/alfonsopisicchio/',
+          tagLine: 'Software Developer', 
+          cardColor: Colors.lightBlue, 
+          companyColor: Colors.red,
+          companyName: 'alter.net', textColor: Colors.white, taglineColor: null,
+        )
       )
     );
+  }
+}
+
+
+
+class AboutMe extends StatelessWidget {
+  //String title="About Me";
+  @override
+  Widget build(BuildContext context){
+    return CustomScrollView(
+      slivers: <Widget>[
+        ContactUs(
+          logo: AssetImage('/my_app/assets/images/ap-logo/AP-dev1.png'),
+          email: 'alfonso.pisicchio@libero.it',
+          githubUserName: 'Forz70043',
+          linkedinURL: 'https://www.linkedin.com/in/alfonsopisicchio/',
+          tagLine: 'Software Developer', 
+          cardColor: Colors.black, 
+          companyColor: Colors.red,
+          companyName: ' ', textColor: Colors.white, taglineColor: null,
+        )
+      ],
+    );
+  }
+
+
+  getBody(){
+  
   }
 }
